1038b314a7b65e109b5de29e07d377ffff34ef7c
xcode <4krutikparikh@gmail.com>
2020-12-17 17:36:10 -0500

    ios configured

--- ios/ShopCA/AppDelegate.m
+++ ios/ShopCA/AppDelegate.m
@@ -6,15 +6,19 @@
  */
 
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
 @implementation AppDelegate
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"ShopCA"
